a2220cab4e0b674a7edba4384a4a58e4ce2bca59
wzr26 <quangdung123.qad@gmail.com>
2026-08-22 04:05:19 +0000

    login

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
+const PORT = 4000;
+
+app.use(express.static(path.join(__dirname, "frontend")));
+
 app.get("/", (req, res) => {
-    res.send("Hello Online Exam");
+    res.send("Trang chủ");
 });
 
-app.listen(4000, () => {
-    console.log("Server running on port 3000");
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "login.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.send("Trang đăng ký");
+});
+
+app.get("/exams", (req, res) => {
+    res.send("Danh sách bài thi");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
